5633ae88ab341611f6b190e3ff658bcff2094a7a
John Ferguson Smart <john.smart@wakaleo.com>
2017-09-05 17:44:14 +0100

    Made the sample project tests pass

--- src/test/java/starter/StepDefinitions.java
+++ src/test/java/starter/StepDefinitions.java
@@ -14,4 +14,8 @@ public class StepDefinitions {
     public void i_order_a_cappuccino() throws Throwable {
     }
 
+    @Then("^I should not receive a latte$")
+    public void i_should_not_receive_a_latte() throws Throwable {
+    }
+
 }
