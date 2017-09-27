6eb215c482a40ec52b8251a703ea6d618cb2cf60
John Ferguson Smart <john.smart@wakaleo.com>
2017-09-27 16:34:16 +0100

    Updated versions

--- src/test/java/starter/StepDefinitions.java
+++ src/test/java/starter/StepDefinitions.java
@@ -1,9 +1,9 @@
 package starter;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 
 public class StepDefinitions {
     @Given("^I am thirsty$")
