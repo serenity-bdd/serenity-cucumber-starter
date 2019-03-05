3875efdf505f0050e939e13d03c9012118d7f9da
John Ferguson Smart <john.smart@wakaleo.com>
2019-03-05 20:36:51 +0530

    Fixed locators

--- src/test/java/starter/search/SearchResultList.java
+++ src/test/java/starter/search/SearchResultList.java
@@ -3,5 +3,5 @@ package starter.search;
 import org.openqa.selenium.By;
 
 class SearchResultList {
-    static By RESULT_TITLES = By.cssSelector(".result__title");
+    static By RESULT_TITLES = By.cssSelector("#links .result__title");
 }
