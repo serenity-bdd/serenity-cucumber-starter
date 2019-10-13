904b14b58380967fda6cb152f2dee49344989659
John Ferguson Smart <john.smart@wakaleo.com>
2019-10-13 11:03:21 +0100

    Updated to serenity-core  2.0.81

--- src/test/java/starter/search/SearchForm.java
+++ src/test/java/starter/search/SearchForm.java
@@ -3,6 +3,6 @@ package starter.search;
 import org.openqa.selenium.By;
 
 class SearchForm {
-    static By SEARCH_FIELD = By.cssSelector(".js-search-input");
-    static By SEARCH_BUTTON = By.cssSelector(".js-search-button");
+    static By SEARCH_FIELD = By.cssSelector("#search_form_input_homepage");
+    static By SEARCH_BUTTON = By.cssSelector("#search_button_homepage");
 }
