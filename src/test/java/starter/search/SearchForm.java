91f93311b84b85fe76690e1aee072fa1df0e67df
John Ferguson Smart <john.smart@wakaleo.com>
2020-01-08 21:00:25 +0000

    updated locators

--- src/test/java/starter/search/SearchForm.java
+++ src/test/java/starter/search/SearchForm.java
@@ -3,6 +3,6 @@ package starter.search;
 import org.openqa.selenium.By;
 
 class SearchForm {
-    static By SEARCH_FIELD = By.cssSelector("#search_form_input_homepage");
-    static By SEARCH_BUTTON = By.cssSelector("#search_button_homepage");
+    static By SEARCH_FIELD = By.name("q");
+    static By SEARCH_BUTTON = By.cssSelector(".search__button");
 }
