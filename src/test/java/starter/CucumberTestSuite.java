e6ec97e3430e5088dfbc1632f445544da96674cc
Loïc PÉRON <loic.peron@gmail.com>
2021-12-21 18:13:58 +0100

    Use cucumber-junit-platform-engine and junit-platform-suite instead of junit4

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -1,12 +1,13 @@
 package starter;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/features"
-)
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "starter")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporter")
+@ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 public class CucumberTestSuite {}
