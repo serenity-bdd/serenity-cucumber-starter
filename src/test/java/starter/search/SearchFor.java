ee6d8913d04357e18d6fbb3a492fd7d625b6492b
Carlos Acevedo <cacevedod@devco.com.co>
2022-09-20 13:18:40 -0500

    change RESULT_TITLES locator and the scenario is corrected

--- src/test/java/starter/search/SearchFor.java
+++ src/test/java/starter/search/SearchFor.java
@@ -5,14 +5,14 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Clear;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import org.openqa.selenium.Keys;
 
 public class SearchFor {
 
     public static Performable term(String term) {
         return Task.where("{0} attempts to search for #term",
                 Clear.field(SearchForm.SEARCH_FIELD),
-                Enter.theValue(term).into(SearchForm.SEARCH_FIELD),
-                Click.on(SearchForm.SEARCH_BUTTON)
+                Enter.theValue(term).into(SearchForm.SEARCH_FIELD).thenHit(Keys.ENTER)
         ).with("term").of(term);
     }
 }
