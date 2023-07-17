c43d2b1ea4adb1481cd5e7bb89bd2add1cc8b281
John Ferguson Smart <john.smart@wakaleo.com>
2023-07-17 13:20:07 +0100

    Updated to 3.9.4

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -1,18 +1,15 @@
 package starter;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.junit.runner.RunWith;
 
-//@RunWith(CucumberWithSerenity.class)
-//@CucumberOptions(
-//        plugin = {"pretty"},
-//        features = "src/test/resources/features"
-//)
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-public class CucumberTestSuite {}
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+public class CucumberTestSuite {
+}
