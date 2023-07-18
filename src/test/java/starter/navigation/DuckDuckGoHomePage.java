6616dbdd243ec77e5a491ea9ed5553f1930099f7
Andrzej Prochyra <andrzej@prochyra.name>
2023-07-18 20:46:27 +0100

    Add missing DuckDuckGoHomePage.java PageObject class

---
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -0,0 +1,8 @@
+package starter.navigation;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("https://duckduckgo.com")
+public class DuckDuckGoHomePage extends PageObject {
+}
