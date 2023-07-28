6b09da1034e78e8fbc3828281757ea15dfc567e2
John Ferguson Smart <john.smart@wakaleo.com>
2023-07-28 17:22:00 +0100

    Added an example using lambda step definitions

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -1,32 +1,28 @@
 package starter.stepdefinitions;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import starter.navigation.NavigateTo;
 import starter.search.LookForInformation;
 
-public class SearchStepDefinitions {
+public class SearchStepDefinitions implements En {
 
-    @Given("{actor} is researching things on the internet")
-    public void researchingThings(Actor actor) {
-        actor.wasAbleTo(NavigateTo.theSearchHomePage());
-    }
+    public SearchStepDefinitions() {
+        Given("{actor} is researching things on the internet", (Actor actor) -> {
+            actor.wasAbleTo(NavigateTo.theSearchHomePage());
+        });
 
-    @When("{actor} looks up {string}")
-    public void searchesFor(Actor actor, String term) {
-        actor.attemptsTo(
-                LookForInformation.about(term)
-        );
-    }
+        When("{actor} looks up {string}", (Actor actor, String term) -> {
+            actor.attemptsTo(LookForInformation.about(term));
+        });
 
-    @Then("{actor} should see information about {string}")
-    public void should_see_information_about(Actor actor, String term) {
-        actor.attemptsTo(
-                Ensure.that(TheWebPage.title()).containsIgnoringCase(term)
-        );
+        Then("{actor} should see information about {string}", (Actor actor, String term) -> {
+            actor.attemptsTo(Ensure.that(TheWebPage.title()).containsIgnoringCase(term));
+        });
     }
 }
